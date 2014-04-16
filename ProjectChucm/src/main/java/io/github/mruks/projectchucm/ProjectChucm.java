0d164c1f8b93ea27c7f20d201318330e446b597b
mruks <misteruks@gmail.com>
2014-04-16 16:37:35 +0200

    added ready, unready and timer commands timer isn't yet implemented teamup addapted for ready and unready commands genmap is more efficient, still contains borderhole bug

--- ProjectChucm/src/main/java/io/github/mruks/projectchucm/ProjectChucm.java
+++ ProjectChucm/src/main/java/io/github/mruks/projectchucm/ProjectChucm.java
@@ -7,11 +7,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.material.MaterialData;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
 
 public final class ProjectChucm extends JavaPlugin {
 	private ConsoleCommandSender console;
@@ -19,6 +23,11 @@ public final class ProjectChucm extends JavaPlugin {
 	private int border = 0;
 	private int plusBorder = 0;
 	private int minBorder = 0;
+	private World world;
+	private int teams = 0;
+	private Scoreboard sb;
+	private ArrayList<Team> ready = new ArrayList<>();
+	private ArrayList<Team> unready = new ArrayList<>();
 
 	private static final String[] TEAM_COLORS = {
 		"black", "dark_blue", "dark_green", "dark_aqua", "dark_red",
@@ -28,14 +37,10 @@ public final class ProjectChucm extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		getLogger().info("onEnable has been invoked!");
-		console = Bukkit.getConsoleSender();
 		Bukkit.setDefaultGameMode(GameMode.ADVENTURE);
-	}
- 
-	@Override
-	public void onDisable() {
-		getLogger().info("onDisable has been invoked!");
+		console = Bukkit.getConsoleSender();
+		world = Bukkit.getWorld("world");
+		sb = Bukkit.getScoreboardManager().getMainScoreboard();
 	}
 
 	@Override
@@ -82,24 +87,34 @@ public final class ProjectChucm extends JavaPlugin {
 			if (args.length < 1) {
 				sender.sendMessage("missing numberOfTeams parameter");
 			} else {
-				int teams = 0;
+				int temp = 0;
 				boolean isParseable = true;				
 				try {
-					teams = Integer.parseInt(args[0]);
+					temp = Integer.parseInt(args[0]);
 				} catch(NumberFormatException e) {
 					isParseable = false;
+					temp = 0;
 				}
 				if (isParseable) {
 					Player[] players = Bukkit.getServer().getOnlinePlayers();
-					if (teams < 2) {
+					if (temp < 2) {
 						sender.sendMessage("You need at least 2 teams to have a good UHC");
-					} else if (teams == players.length) {
+					} else if (temp == players.length) {
 						sender.sendMessage("If everyone is alone, no teams are needed ;)");
-					} else if (teams > 16) {
+					} else if (temp > 16) {
 						sender.sendMessage("You can't have more teams than colors(16) available");
-					} else if (teams > players.length) {
+					} else if (temp > players.length) {
 						sender.sendMessage("You can't have more teams than players");
 					} else {
+						if (teams > 0) {
+							ready.clear();
+							unready.clear();
+							for (int i = 1; i <= teams; i++) {
+								Bukkit.dispatchCommand(console, "scoreboard teams empty team" + i);
+								Bukkit.dispatchCommand(console, "scoreboard teams remove team" + i);
+							}
+						}
+						teams = temp;
 						int playersPerTeam = players.length / teams;
 						int rest = players.length % teams;
 						ArrayList<String> colors = new ArrayList<>();
@@ -112,11 +127,13 @@ public final class ProjectChucm extends JavaPlugin {
 							for (int i = 0; i < rest; i++) {
 								Bukkit.dispatchCommand(console, "scoreboard teams join team" + rand.nextInt(teams) + " @r[c=1,team=]");
 							}
+							teams--;
 						} else {
 							Bukkit.dispatchCommand(console, "scoreboard teams add team" + teams);
 							Bukkit.dispatchCommand(console, "scoreboard teams option color "+getRandomColor(colors));
 							Bukkit.dispatchCommand(console, "scoreboard teams join team" + teams + " @r[c=" + rest + ",team=]");
 						}
+						unready.addAll(sb.getTeams());
 					}
 				} else {
 					sender.sendMessage("NumberOfTeams has to be a valid number");
@@ -129,48 +146,23 @@ public final class ProjectChucm extends JavaPlugin {
 				} else {
 					((Player)sender).setFlying(true);
 					Bukkit.dispatchCommand(console, "tp " + sender.getName() + " " + minBorder + " 200 " + minBorder);
-					World world = Bukkit.getWorld("world");
 					if (world.getChunkAt(minBorder, minBorder).isLoaded()) {
 						world.loadChunk(minBorder,minBorder);
 						for (Player p : Bukkit.getServer().getOnlinePlayers()) {
 							Bukkit.dispatchCommand(console, "kick " + p.getName() + " generating borders for UHC is very laginducing, you'll only make it worse :p\nPlease don't rejoin the server until it succesfully termimnates itself!");
 						}
-						//gen actual borders
 						console.sendMessage("Starting border generation: ...");
-						int fullPercent = border*1024;
-						int percent = 0;
-						for (int i = minBorder; i <= plusBorder; i++) {
-							world.loadChunk(i,minBorder,false);
-							for (int j = 0; j < 256; j++) {
-								world.getBlockAt(i,j,minBorder).setType(Material.BEDROCK);
-							}
-							percent+=256;
-							if (i%10==0) console.sendMessage("Border generation at: " + (int)(percent*100/fullPercent) + "%");
-						}
-						for (int i = minBorder; i <= plusBorder; i++) {
-							world.loadChunk(plusBorder,i,false);
-							for (int j = 0; j < 256; j++) {
-								world.getBlockAt(plusBorder,j,i).setType(Material.BEDROCK);
-							}
-							percent+=256;
-							if (i%10==0) console.sendMessage("Border generation at: " + (int)(percent*100/fullPercent) + "%");
-						}
-						for (int i = plusBorder; i >= minBorder; i--) {
-							world.loadChunk(i,plusBorder,false);
-							for (int j = 0; j < 256; j++) {
-								world.getBlockAt(i,j,plusBorder).setType(Material.BEDROCK);
-							}
-							percent+=256;
-							if (i%10==0) console.sendMessage("Border generation at: " + (int)(percent*100/fullPercent) + "%");
-						}
-						for (int i = plusBorder; i >= minBorder; i--) {
-							world.loadChunk(minBorder,i,false);
-							for (int j = 0; j < 256; j++) {
-								world.getBlockAt(minBorder,j,i).setType(Material.BEDROCK);
-							}
-							percent+=256;
-							if (i%10==0) console.sendMessage("Border generation at: " + (int)(percent*100/fullPercent) + "%");
-						}
+						int percent[] = {0,border*1024};
+//						//terain gen
+//						for (int i = minBorder; i <= plusBorder; i+=16) loadBorders(i,minBorder,percent);
+//						for (int i = minBorder; i <= plusBorder; i+=16) loadBorders(plusBorder,i,percent);
+//						for (int i = plusBorder; i >= minBorder; i-=16) loadBorders(i,plusBorder,percent);
+//						for (int i = plusBorder; i >= minBorder; i-=16) loadBorders(minBorder,i,percent);
+						//gen actual borders
+						for (int i = minBorder; i <= plusBorder; i++) setBorders(i, minBorder, percent);
+						for (int i = minBorder; i <= plusBorder; i++) setBorders(plusBorder,i, percent);
+						for (int i = plusBorder; i >= minBorder; i--) setBorders(i, plusBorder, percent);
+						for (int i = plusBorder; i >= minBorder; i--) setBorders(minBorder,i, percent);
 						console.sendMessage("Server needs to restart");
 						Bukkit.dispatchCommand(console, "stop");
 					} else {
@@ -180,8 +172,69 @@ public final class ProjectChucm extends JavaPlugin {
 			} else {
 				sender.sendMessage("This command has to be performed by a logged on OP in order to work");
 			}
+		} else if (cmd.equals("ready")) {
+			if (sender instanceof Player) {
+				Player player = (Player)sender;
+				Team team = null;
+				for (Team temp : unready) {
+					if (temp.getPlayers().contains(player)) {
+						unready.remove(temp);
+						ready.add(temp);
+						team = temp;
+						break;
+					}
+				}
+				if (team == null) sender.sendMessage("Your team is already ready, or you're not in a team");
+				else console.sendMessage(team.getDisplayName() + " is now ready");
+			} else {
+				sender.sendMessage("This command has to be performed by a player in order to work");
+			}
+		} else if (cmd.equals("unready")) {
+			if (sender instanceof Player) {
+				Player player = (Player)sender;
+				Team team = null;
+				for (Team temp : ready) {
+					if (temp.getPlayers().contains(player)) {
+						ready.remove(temp);
+						unready.add(temp);
+						team = temp;
+						break;
+					}
+				}
+				if (team == null) sender.sendMessage("Your team wasn't yet ready, or you're not in a team");
+				else console.sendMessage(team.getDisplayName() + " wussed out");
+			} else {
+				sender.sendMessage("This command has to be performed by a player in order to work");
+			}
+		} else if (cmd.equals("timer")) {
 		}
 		return super.onCommand(sender, command, label, args);
+	}
+	
+//	private void loadBorders(int x, int z, int[] percent) {
+//		world.loadChunk(x,z,true);
+//		world.refreshChunk(x,z);
+//		world.regenerateChunk(x,z);
+//		world.refreshChunk(x,z);
+//		setBorders(x, z, percent);
+//		percentPrint(percent);
+//	}
+	
+	private void setBorders(int x, int z, int[] percent) {
+		world.loadChunk(x,z,true);
+		for (int i = 0; i < 256; i++) {
+			Block block = world.getBlockAt(x,i,z);
+			block.getState().setData(new MaterialData(Material.BEDROCK));
+			block.setType(Material.BEDROCK);
+		}
+		percentPrint(percent);
+	}
+
+	private void percentPrint(int[] percent) {
+		percent[0]+=256;
+		if (percent[0]%10000==0) {
+			console.sendMessage("Border generation at: " + (int)(percent[0]*100/percent[1]) + "%");
+		}
 	}
 	
 	private String getRandomColor(ArrayList<String> list) {
